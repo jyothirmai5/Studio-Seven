945e0ef4d4de41b1c0d056188c1786d61083a073
jyothirmai5 <chjyothirmai5@gmail.com>
2023-10-05 18:47:23 -0400

    Added packages and added folders and files as per the user flow

---
+++ src/CategoryList/CategoryList.js
@@ -0,0 +1,5 @@
+function CategoryList() {
+    return (<h1>CategoryList</h1>);
+}
+
+export default CategoryList;
---
+++ src/ProductsList/ProductsList.js
@@ -0,0 +1,5 @@
+function ProductList() {
+    return (<h1>ProductList</h1>);
+}
+
+export default ProductList;
