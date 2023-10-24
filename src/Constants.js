341a7ebec75fd2f4bd76bd3f07d187e455873c18
[firstname lastname] <megantariaisyah@gmail.com>
2023-10-24 10:39:29 -0400

    I added product description for decor collection

--- src/Constants.js
+++ src/Constants.js
@@ -6,10 +6,52 @@ export const categories = [
   {
     title: 'Decor',
     imageUrl: decorImage,
+    product: [  
+      {
+      tilte:'Kids Girrafe Rug',
+      image:'',
+      description:'Safari Dreams: Kids Giraffe Rugs Collection. These vibrant and playful rugs bring the magic of the savannah right into your kids room. Crafted with care, each rug is not only an exciting decorative piece but also a cozy haven for little feet.',
+      price:'95$'
+    }, 
+    {
+      tilte:'Rainbow Wall Lamp',
+      image:'',
+      description:'Designed with both aesthetics and safety in mind, a magical addition to your kids room that will turn their space into a whimsical and calming atmosphere for bedtime stories and sweet dreams..',
+      price:'65$'
+    }, 
+    {
+      tilte:'Soothing Blue-  Kids Wall Decor Set with Frames, Photos, and Books',
+      image:'',
+      description:'Elevate the look of your kids room with our exquisite wall decor set. Featuring a perfectly picked beautiful doll from our best collection , stylish frames for your cherished photos, and a selection of captivating books',
+      price:'150$'
+    },  
+    {
+      tilte:'Enchanted Playtime: Deluxe Wall Decor Collection',
+      image:'',
+      description:'Deluxe Wall Decor Collection - a grand ensemble of handpicked decor elements that will transform kids playroom into a haven of play and organization.',
+      price:'1350$'
+    },  
+    {
+      tilte:'Foxy Adventure: Kids - Wall Decor Set with Adorable Fox Theme',
+      image:'',
+      description:'This collection is a perfect addition, it creates a sense of wonder and imagination to your kids space. This set includes charming wall art, vibrant colors, and adorable fox dolls from our best collection. The fox theme invites an imaginative journeys and playtime adventures.',
+      price:'150$'
+    }, 
+
+    ]
   },
+
   {
     title: 'Bedroom',
-    imageUrl: bedroomImage,
+    imageUrl: bedroomImage
+    product: [
+      {
+        tilte:'Kids Girrafe Rug',
+        image:'',
+        description:'Safari Dreams: Kids Giraffe Rugs Collection. These vibrant and playful rugs bring the magic of the savannah right into your kids room. Crafted with care, each rug is not only an exciting decorative piece but also a cozy haven for little feet.',
+        price:'95$'
+      }
+    ]
   },
   {
     title: 'Play room',
