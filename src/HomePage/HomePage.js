4f7676ee3dbb17f3959cc6855ec1e9c6b20e4576
[firstname lastname] <megantariaisyah@gmail.com>
2023-10-25 21:17:43 -0400

    I layouted the product details

--- src/HomePage/HomePage.js
+++ src/HomePage/HomePage.js
@@ -1,51 +1,50 @@
-import Layout from '../Layout/Layout';
-import SlickCarousel from '../SlickCarousel/SlickCarousel';
-import { makeStyles } from '@mui/styles';
-import {
-    Paper,
-    Grid,
-    Typography,
-} from '@mui/material';
-import { categories } from '../Constants';
+import Layout from "../Layout/Layout";
+import SlickCarousel from "../SlickCarousel/SlickCarousel";
+import { makeStyles } from "@mui/styles";
+import { Paper, Grid, Typography } from "@mui/material";
+import "./HomePage.css";
+import { categories } from "../Constants";
 
 const useStyles = makeStyles((theme) => ({
-    categoryCard: {
-        height: '200px',
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'center',
-        backgroundSize: 'cover',
-        backgroundRepeat: 'no-repeat',
-        backgroundPosition: 'center center',
-        '&:hover': {
-            cursor: 'pointer',
-            backgroundColor: 'rgba(0, 0, 0, 0.5)',
-        },
+  categoryCard: {
+    height: "200px",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat",
+    backgroundPosition: "center center",
+    "&:hover": {
+      cursor: "pointer",
+      backgroundColor: "rgba(0, 0, 0, 0.5)",
     },
-    categoryTitle: {
-        color: 'white',
-    },
+  },
+  categoryTitle: {
+    color: "white",
+  },
 }));
 function HomePage() {
-    const classes = useStyles();
-    return (<Layout>
-        <SlickCarousel />
-        <Grid container spacing={2}>
-            {categories.map((category, index) => (
-                <Grid item xs={12} sm={6} md={3} key={index}>
-                    <Paper
-                        className={classes.categoryCard}
-                        style={{ backgroundImage: `url(${category.imageUrl})` }}
-                    >
-                        <Typography variant="h6" className={classes.categoryTitle}>
-                            {category.title}
-                        </Typography>
-                    </Paper>
-                </Grid>
-            ))}
-        </Grid>
-    </Layout>);
+  const classes = useStyles();
+  return (
+    <Layout>
+      <SlickCarousel />
+      <Grid container spacing={2}>
+        {categories.map((category, index) => (
+          <Grid item xs={12} sm={6} md={3} key={index}>
+            <Paper
+              className={classes.categoryCard}
+              style={{ backgroundImage: `url(${category.imageUrl})` }}
+            >
+              <Typography variant="h6" className={classes.categoryTitle}>
+                {category.title}
+              </Typography>
+            </Paper>
+          </Grid>
+        ))}
+      </Grid>
+    </Layout>
+  );
 }
 
 export default HomePage;
