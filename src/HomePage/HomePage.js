8eaec10cf747ddbb6ffdff9785bec80a44586b07
{chigashi65} <{chigashi65@gmail.com}>
2023-11-07 13:21:11 -0500

    I changed HomePage.js to adjust category styling

--- src/HomePage/HomePage.js
+++ src/HomePage/HomePage.js
@@ -12,23 +12,28 @@ import SalePc from "../assets/banner/offer_banner_pc/studioseven_offer_banner1.g
 import "./HomePage.css";
 
 const useStyles = makeStyles((theme) => ({
-  categoryCard: {
-    height: "200px",
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    justifyContent: "center",
-    backgroundSize: "cover",
-    backgroundRepeat: "no-repeat",
-    backgroundPosition: "center center",
-    "&:hover": {
-      cursor: "pointer",
-      backgroundColor: "rgba(0, 0, 0, 0.5)",
+    categoryCard: {
+        height: "400px",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "space-between",
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "center center",
+        "&:hover": {
+            cursor: "pointer",
+            backgroundColor: "rgba(0, 0, 0, 0.5)",
+        },
     },
-  },
-  categoryTitle: {
-    color: "white",
-  },
+    categoryTitle: {
+        color: "#58869e",
+        textAlign: "center",
+        paddingTop: "20px",
+        fontSize:"1rem"
+    },
+
+
 }));
 function HomePage() {
     const classes = useStyles();
@@ -40,29 +45,31 @@ function HomePage() {
         <SlickCarousel />
         <div className='main-content'>
             <div className='hp-sale'>
-                <div  className='sale-title'>
+                <div className='sale-title'>
                     <h1>SALE</h1>
                 </div>
                 <div className='sale-pc'><img src={SalePc} className="sale-image" alt="sale" onClick={() => navigate('/')}></img></div>
-            
+
             </div>
-            <div  className='category-title'>
-                    <h1>CATEGORY</h1>
+            <div className='category-title'>
+                <h1>CATEGORY</h1>
             </div>
+            <div class="category-content">
                 <Grid container spacing={2}>
                     {categories.map((category, index) => (
                         <Grid item xs={12} sm={6} md={3} key={index} onClick={() => redirectToProducts(category.title)}>
                             <Paper
                                 className={classes.categoryCard}
                                 style={{ backgroundImage: `url(${category.imageUrl})` }}
-                            >
-                                <Typography variant="h6" className={classes.categoryTitle}>
-                                    {category.title}
-                                </Typography>
-                            </Paper>
+                            ></Paper>
+                            <Typography variant="h4" className={classes.categoryTitle}>
+                                {category.title}
+                            </Typography>
+
                         </Grid>
                     ))}
                 </Grid>
+            </div>
         </div>
     </Layout>);
 }
