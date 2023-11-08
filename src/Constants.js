c7550116c17e5e4e66838b5006e3c8df0d1c6bf5
jyothirmai5 <chjyothirmai5@gmail.com>
2023-11-08 13:57:27 -0500

    Added playroom products

--- src/Constants.js
+++ src/Constants.js
@@ -11,79 +11,83 @@ export const categories = [
         id: "D1",
         tilte: 'Kids Girrafe Rug',
         image: ['Decor/D1/image1.jpeg', 'Decor/D1/image2.jpeg'],
-        description: 'Safari Dreams: Kids Giraffe Rugs Collection. These vibrant and playful rugs bring the magic of the savannah right into your kids room. Crafted with care, each rug is not only an exciting decorative piece but also a cozy haven for little feet.',
+        description: "Safari Dreams: Kid's Giraffe Rugs Collection. These vibrant and playful rugs bring the magic of the savannah right into your Kid's room. Crafted with care, each rug is not only an exciting decorative piece but also a cozy haven for little feet.",
         price: '95'
       },
       {
         id: "D2",
         tilte: 'Rainbow Wall Lamp',
         image: ['Decor/D2/image1.jpeg', 'Decor/D2/image2.jpeg'],
-        description: 'Designed with both aesthetics and safety in mind, a magical addition to your kids room that will turn their space into a whimsical and calming atmosphere for bedtime stories and sweet dreams..',
+        description: "Designed with both aesthetics and safety in mind, a magical addition to your kids room that will turn their space into a whimsical and calming atmosphere for bedtime stories and sweet dreams..",
         price: '65'
       },
       {
         id: "D3",
         tilte: 'Soothing Blue-  Kids Wall Decor Set with Frames, Photos, and Books',
         image: ['Decor/D3/image1.jpeg', 'Decor/D3/image2.jpeg'],
-        description: " Elevate the look of your kid's room with our exquisite wall decor set. Featuring a perfectly picked beautiful doll from our best collection , stylish frames for your cherished photos, and a selection of captivating books",
+        description: "Elevate the look of your Kid's room with our exquisite wall decor set. Featuring a perfectly picked beautiful doll from our best collection , stylish frames for your cherished photos, and a selection of captivating books",
         price: '150'
       },
       {
         id: "D4",
         tilte: 'Enchanted Playtime: Deluxe Wall Decor Collection',
         image: ['Decor/D4/image1.jpeg', 'Decor/D4/image2.jpeg'],
-        description: 'Deluxe Wall Decor Collection - a grand ensemble of handpicked decor elements that will transform kids playroom into a haven of play and organization.',
+        description: "Deluxe Wall Decor Collection - a grand ensemble of handpicked decor elements that will transform kids playroom into a haven of play and organization.",
         price: '1350'
       },
       {
         id: "D5",
         tilte: 'Foxy Adventure: Kids - Wall Decor Set with Adorable Fox Theme',
         image: ['Decor/D5/image1.jpeg', 'Decor/D5/image2.jpeg'],
-        description: 'This collection is a perfect addition, it creates a sense of wonder and imagination to your kids space. This set includes charming wall art, vibrant colors, and adorable fox dolls from our best collection. The fox theme invites an imaginative journeys and playtime adventures.',
+        description: "This collection is a perfect addition, it creates a sense of wonder and imagination to your kids space. This set includes charming wall art, vibrant colors, and adorable fox dolls from our best collection. The fox theme invites an imaginative journeys and playtime adventures.",
         price: '150'
       },
     ]
   },
-
   {
     title: "Bedroom",
     imageUrl: bedroomImage,
     product: [
       {
         id: "B1",
-        tilte: 'Minimalist Kids Bed - Twin size',
+        tilte: "WallHug Twin Size Bed with Cozy Puff Side and Headrest",
         image: ['Bedroom/B1/image1.jpeg', 'Bedroom/B1/image2.jpeg'],
-        description: 'This collection is a perfect addition, it creates a sense of wonder and imagination to your kids space. This set includes charming wall art, vibrant colors, and adorable fox dolls from our best collection. The fox theme invites an imaginative journeys and playtime adventures.',
-        price: '1075'
+        description:
+          "Elevate your child's bedroom with our Twin Wall-Mounted Bed. This innovative space-saving solution attaches securely to the wall, offering ample floor space. The included side and headrest puff cushions ensure both comfort and style.",
+        price: "$4.999,00",
       },
       {
         id: "B2",
-        tilte: 'Minimalist Kids Bed - Twin size',
+        tilte: "Cloudy Dreams Twin Bed: Versatile Sofa and Kid's Bed",
         image: ['Bedroom/B2/image1.jpeg', 'Bedroom/B2/image2.jpeg'],
-        description: 'This collection is a perfect addition, it creates a sense of wonder and imagination to your kids space. This set includes charming wall art, vibrant colors, and adorable fox dolls from our best collection. The fox theme invites an imaginative journeys and playtime adventures.',
-        price: '1075'
+        description:
+          "Presenting the Cloudy Dreams Twin Bed, a versatile addition to any child's room. This twin-size bed seamlessly transforms into a comfortable sofa during the day, complemented by a cloud-shaped headrest that adds a touch of whimsy and comfort.",
+        price: "$4.499,00",
       },
       {
         id: "B3",
-        tilte: 'Minimalist Kids Bed - Twin size',
+        tilte: "Slide & Store Twin Bed with Storage and Pretty Pink Headrest",
         image: ['Bedroom/B3/image1.jpeg', 'Bedroom/B3/image2.jpeg'],
-        description: 'This collection is a perfect addition, it creates a sense of wonder and imagination to your kids space. This set includes charming wall art, vibrant colors, and adorable fox dolls from our best collection. The fox theme invites an imaginative journeys and playtime adventures.',
-        price: '1075'
+        description:
+          "Our Twin Bed with Built-In Slide and Storage Stairs offers both functionality and charm. The pink headrest adds a delightful accent to the room, and the creme paint finish harmonizes with various bedroom decor styles.",
+        price: "$14.499,00",
       },
       {
         id: "B4",
-        tilte: 'Minimalist Kids Bed - Twin size',
+        tilte: "Slide & Store Twin Bed with Storage and Neutral Grey Headrest",
         image: ['Bedroom/B4/image1.jpeg', 'Bedroom/B4/image2.jpeg'],
-        description: 'This collection is a perfect addition, it creates a sense of wonder and imagination to your kids space. This set includes charming wall art, vibrant colors, and adorable fox dolls from our best collection. The fox theme invites an imaginative journeys and playtime adventures.',
-        price: '1075'
+        description:
+          "Transform your child's room with our Twin Bed featuring a built-in slide and storage stairs. The combination of pink and grey headrests adds a modern touch to the bedroom, while the creme and sage green paint finish complements the overall look.",
+        price: "$12.499,00",
       },
       {
         id: "B5",
-        tilte: 'Minimalist Kids Bed - Twin size',
+        tilte: "Bunny Dreams Single Bed Frame in Pink and Green Suede",
         image: ['Bedroom/B5/image1.jpeg', 'Bedroom/B5/image2.jpeg'],
-        description: 'This collection is a perfect addition, it creates a sense of wonder and imagination to your kids space. This set includes charming wall art, vibrant colors, and adorable fox dolls from our best collection. The fox theme invites an imaginative journeys and playtime adventures.',
-        price: '1075'
-      }
+        description:
+          "Create a magical and cozy atmosphere in your child's room with our Single Bed Frame featuring a bunny headrest shape. It is available in both pink and green suede fabric, allowing your child to choose their preferred color. This bed frame is a perfect addition to any child's room, providing warmth and comfort.",
+        price: "$6.499,00",
+      },
     ]
   },
   {
@@ -93,35 +97,35 @@ export const categories = [
       {
         id: "P1",
         tilte: 'Minimalist Kids Bed - Twin size',
-        image: ['Bedroom/B1/image1.jpeg', 'Bedroom/B1/image2.jpeg'],
+        image: ['Playroom/P1/image1.jpeg', 'Playroom/P1/image2.jpeg'],
         description: 'This collection is a perfect addition, it creates a sense of wonder and imagination to your kids space. This set includes charming wall art, vibrant colors, and adorable fox dolls from our best collection. The fox theme invites an imaginative journeys and playtime adventures.',
         price: '1075'
       },
       {
         id: "P2",
         tilte: 'Minimalist Kids Bed - Twin size',
-        image: ['Bedroom/B2/image1.jpeg', 'Bedroom/B2/image2.jpeg'],
+        image: ['Playroom/P2/image1.jpeg', 'Playroom/P2/image2.jpeg'],
         description: 'This collection is a perfect addition, it creates a sense of wonder and imagination to your kids space. This set includes charming wall art, vibrant colors, and adorable fox dolls from our best collection. The fox theme invites an imaginative journeys and playtime adventures.',
         price: '1075'
       },
       {
         id: "P3",
         tilte: 'Minimalist Kids Bed - Twin size',
-        image: ['Bedroom/B3/image1.jpeg', 'Bedroom/B3/image2.jpeg'],
+        image: ['Playroom/P3/image1.jpeg', 'Playroom/P3/image2.jpeg'],
         description: 'This collection is a perfect addition, it creates a sense of wonder and imagination to your kids space. This set includes charming wall art, vibrant colors, and adorable fox dolls from our best collection. The fox theme invites an imaginative journeys and playtime adventures.',
         price: '1075'
       },
       {
         id: "P4",
         tilte: 'Minimalist Kids Bed - Twin size',
-        image: ['Bedroom/B4/image1.jpeg', 'Bedroom/B4/image2.jpeg'],
+        image: ['Playroom/P4/image1.jpeg', 'Playroom/P4/image2.jpeg'],
         description: 'This collection is a perfect addition, it creates a sense of wonder and imagination to your kids space. This set includes charming wall art, vibrant colors, and adorable fox dolls from our best collection. The fox theme invites an imaginative journeys and playtime adventures.',
         price: '1075'
       },
       {
         id: "P5",
         tilte: 'Minimalist Kids Bed - Twin size',
-        image: ['Bedroom/B5/image1.jpeg', 'Bedroom/B5/image2.jpeg'],
+        image: ['Playroom/P5/image1.jpeg', 'Playroom/P5/image2.jpeg'],
         description: 'This collection is a perfect addition, it creates a sense of wonder and imagination to your kids space. This set includes charming wall art, vibrant colors, and adorable fox dolls from our best collection. The fox theme invites an imaginative journeys and playtime adventures.',
         price: '1075'
       }
--- src/Favourites/Favourites.js
+++ src/Favourites/Favourites.js
@@ -1,11 +1,59 @@
 import React, { useEffect, useState } from 'react';
+import { Card, CardContent, CardMedia, Typography, Container, Grid, Button, IconButton } from '@mui/material';
+import { categories } from '../Constants';
+import { useParams, useNavigate } from 'react-router-dom';
 import Layout from '../Layout/Layout';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 
-function Favourites() {
+const Favourites = () => {
+    const { category } = useParams();
+    const [products, setProducts] = useState([]);
+    const navigate = useNavigate();
+    const redirectToProductDetails = (id) => {
+        navigate('/' + category + '/products/' + id);
+    }
+    useEffect(() => {
+        categories.map((item) => {
+            if (item.title === category) {
+                setProducts(item.product)
+            }
+        })
+    }, [category]);
+
     return (
         <Layout>
-            <h1>Favourites</h1>
-        </Layout>);
-}
+            <Container sx={{ padding: '40px' }}>
+                <IconButton sx={{ mb: 2, color: '#58869e' }} onClick={() => navigate('/')}>
+                    <ArrowBackIcon />
+                </IconButton>
+                <Grid container spacing={2}>
+                    {products && products.map((product, index) => (
+                        <Grid item xs={12} sm={6} md={4} key={index}>
+                            <Card>
+                                <CardMedia
+                                    component="img"
+                                    height="250"
+                                    image={require(`../assets/${product.image[0]}`)}
+                                    alt={product.name}
+                                />
+                                <CardContent>
+                                    <Typography variant="h6" component="div">
+                                        {product.name}
+                                    </Typography>
+                                    <Typography variant="body2" color="text.secondary">
+                                        Price: ${product.price}
+                                    </Typography>
+                                    <Button variant="contained" color="primary" onClick={() => redirectToProductDetails(product.id)}>
+                                        Show more
+                                    </Button>
+                                </CardContent>
+                            </Card>
+                        </Grid>
+                    ))}
+                </Grid>
+            </Container>
+        </Layout>
+    );
+};
 
 export default Favourites;
