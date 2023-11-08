38b8b17db349ea990c9ed9ee82da005dbe779cb3
jyothirmai5 <chjyothirmai5@gmail.com>
2023-11-08 15:16:05 -0500

    I have added cart items logic

--- src/Constants.js
+++ src/Constants.js
@@ -12,35 +12,35 @@ export const categories = [
         tilte: 'Kids Girrafe Rug',
         image: ['Decor/D1/image1.jpeg', 'Decor/D1/image2.jpeg'],
         description: "Safari Dreams: Kid's Giraffe Rugs Collection. These vibrant and playful rugs bring the magic of the savannah right into your Kid's room. Crafted with care, each rug is not only an exciting decorative piece but also a cozy haven for little feet.",
-        price: '95'
+        price: 95
       },
       {
         id: "D2",
         tilte: 'Rainbow Wall Lamp',
         image: ['Decor/D2/image1.jpeg', 'Decor/D2/image2.jpeg'],
         description: "Designed with both aesthetics and safety in mind, a magical addition to your kids room that will turn their space into a whimsical and calming atmosphere for bedtime stories and sweet dreams..",
-        price: '65'
+        price: 65
       },
       {
         id: "D3",
         tilte: 'Soothing Blue-  Kids Wall Decor Set with Frames, Photos, and Books',
         image: ['Decor/D3/image1.jpeg', 'Decor/D3/image2.jpeg'],
         description: "Elevate the look of your Kid's room with our exquisite wall decor set. Featuring a perfectly picked beautiful doll from our best collection , stylish frames for your cherished photos, and a selection of captivating books",
-        price: '150'
+        price: 150
       },
       {
         id: "D4",
         tilte: 'Enchanted Playtime: Deluxe Wall Decor Collection',
         image: ['Decor/D4/image1.jpeg', 'Decor/D4/image2.jpeg'],
         description: "Deluxe Wall Decor Collection - a grand ensemble of handpicked decor elements that will transform kids playroom into a haven of play and organization.",
-        price: '1350'
+        price: 1350
       },
       {
         id: "D5",
         tilte: 'Foxy Adventure: Kids - Wall Decor Set with Adorable Fox Theme',
         image: ['Decor/D5/image1.jpeg', 'Decor/D5/image2.jpeg'],
         description: "This collection is a perfect addition, it creates a sense of wonder and imagination to your kids space. This set includes charming wall art, vibrant colors, and adorable fox dolls from our best collection. The fox theme invites an imaginative journeys and playtime adventures.",
-        price: '150'
+        price: 150
       },
     ]
   },
@@ -54,7 +54,7 @@ export const categories = [
         image: ['Bedroom/B1/image1.jpeg', 'Bedroom/B1/image2.jpeg'],
         description:
           "Elevate your child's bedroom with our Twin Wall-Mounted Bed. This innovative space-saving solution attaches securely to the wall, offering ample floor space. The included side and headrest puff cushions ensure both comfort and style.",
-        price: "$4.999,00",
+        price: 4.999,
       },
       {
         id: "B2",
@@ -62,7 +62,7 @@ export const categories = [
         image: ['Bedroom/B2/image1.jpeg', 'Bedroom/B2/image2.jpeg'],
         description:
           "Presenting the Cloudy Dreams Twin Bed, a versatile addition to any child's room. This twin-size bed seamlessly transforms into a comfortable sofa during the day, complemented by a cloud-shaped headrest that adds a touch of whimsy and comfort.",
-        price: "$4.499,00",
+        price: 4.499,
       },
       {
         id: "B3",
@@ -70,7 +70,7 @@ export const categories = [
         image: ['Bedroom/B3/image1.jpeg', 'Bedroom/B3/image2.jpeg'],
         description:
           "Our Twin Bed with Built-In Slide and Storage Stairs offers both functionality and charm. The pink headrest adds a delightful accent to the room, and the creme paint finish harmonizes with various bedroom decor styles.",
-        price: "$14.499,00",
+        price: 14.499,
       },
       {
         id: "B4",
@@ -78,7 +78,7 @@ export const categories = [
         image: ['Bedroom/B4/image1.jpeg', 'Bedroom/B4/image2.jpeg'],
         description:
           "Transform your child's room with our Twin Bed featuring a built-in slide and storage stairs. The combination of pink and grey headrests adds a modern touch to the bedroom, while the creme and sage green paint finish complements the overall look.",
-        price: "$12.499,00",
+        price: 12.499,
       },
       {
         id: "B5",
@@ -86,7 +86,7 @@ export const categories = [
         image: ['Bedroom/B5/image1.jpeg', 'Bedroom/B5/image2.jpeg'],
         description:
           "Create a magical and cozy atmosphere in your child's room with our Single Bed Frame featuring a bunny headrest shape. It is available in both pink and green suede fabric, allowing your child to choose their preferred color. This bed frame is a perfect addition to any child's room, providing warmth and comfort.",
-        price: "$6.499,00",
+        price: 6.499,
       },
     ]
   },
@@ -98,36 +98,40 @@ export const categories = [
         id: "P1",
         tilte: 'Minimalist Kids Bed - Twin size',
         image: ['Playroom/P1/image1.jpeg', 'Playroom/P1/image2.jpeg'],
-        description: 'This collection is a perfect addition, it creates a sense of wonder and imagination to your kids space. This set includes charming wall art, vibrant colors, and adorable fox dolls from our best collection. The fox theme invites an imaginative journeys and playtime adventures.',
-        price: '1075'
+        description:
+          "Introducing our Little Explorers' Playtime Toy Set, designed for 3-year-old kids with an appetite for play and imagination. This set includes a collection of cars, carts, small trains, and cars toys that match the holes for endless creative fun. The soft, neutral color palette complements any home decor, making it not just a delightful playset but also a stylish addition to your living space.",
+        price: 9.999,
       },
       {
         id: "P2",
         tilte: 'Minimalist Kids Bed - Twin size',
         image: ['Playroom/P2/image1.jpeg', 'Playroom/P2/image2.jpeg'],
-        description: 'This collection is a perfect addition, it creates a sense of wonder and imagination to your kids space. This set includes charming wall art, vibrant colors, and adorable fox dolls from our best collection. The fox theme invites an imaginative journeys and playtime adventures.',
-        price: '1075'
+        description:
+          "Give your little one the thrill of independence with our Tiny Cyclist's Wooden Bike. Specifically designed for 3-year-old kids, this charming wooden bike is a great way to help them develop balance and coordination. The leg-operated design ensures a safe and enjoyable riding experience for your child as they embark on their first biking adventures.",
+        price: 99.99,
       },
       {
         id: "P3",
         tilte: 'Minimalist Kids Bed - Twin size',
         image: ['Playroom/P3/image1.jpeg', 'Playroom/P3/image2.jpeg'],
-        description: 'This collection is a perfect addition, it creates a sense of wonder and imagination to your kids space. This set includes charming wall art, vibrant colors, and adorable fox dolls from our best collection. The fox theme invites an imaginative journeys and playtime adventures.',
-        price: '1075'
+        description:
+          "Encourage your child's creativity and fine motor skills with our Creative Builders' Wooden Lego Set. This set features a variety of wooden building blocks, providing endless possibilities for construction and imaginative play. Whether they're building towers, houses, or whatever their hearts desire, this set is designed to foster learning through play.",
+        price: 129.99,
       },
       {
         id: "P4",
         tilte: 'Minimalist Kids Bed - Twin size',
         image: ['Playroom/P4/image1.jpeg', 'Playroom/P4/image2.jpeg'],
-        description: 'This collection is a perfect addition, it creates a sense of wonder and imagination to your kids space. This set includes charming wall art, vibrant colors, and adorable fox dolls from our best collection. The fox theme invites an imaginative journeys and playtime adventures.',
-        price: '1075'
+        description: "The simplicity of our Elephant Dreams Kids' Table and Chairs Set brings a touch of whimsy to any child's playroom or bedroom. With adorable elephant-shaped chairs and a wooden table, this set is perfect for activities, arts and crafts, or snack time. The charming design and sturdy construction make it a delightful addition to your child's space.",
+        price: 59.99,
       },
       {
         id: "P5",
         tilte: 'Minimalist Kids Bed - Twin size',
         image: ['Playroom/P5/image1.jpeg', 'Playroom/P5/image2.jpeg'],
-        description: 'This collection is a perfect addition, it creates a sense of wonder and imagination to your kids space. This set includes charming wall art, vibrant colors, and adorable fox dolls from our best collection. The fox theme invites an imaginative journeys and playtime adventures.',
-        price: '1075'
+        description:
+          "The simplicity of our Elephant Dreams Kids' Table and Chairs Set brings a touch of whimsy to any child's playroom or bedroom. With adorable elephant-shaped chairs and a wooden table, this set is perfect for activities, arts and crafts, or snack time. The charming design and sturdy construction make it a delightful addition to your child's space.",
+        price: 299.00,
       }
     ]
   }
--- src/ProductDetails/ProductDetails.js
+++ src/ProductDetails/ProductDetails.js
@@ -5,14 +5,32 @@ import "./ProductDetails.css";
 import Layout from '../Layout/Layout';
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import { IconButton } from "@mui/material";
+import { Store } from '../Context';
+import Snackbar from '@mui/material/Snackbar';
+import MuiAlert from '@mui/material/Alert';
 
+const Alert = React.forwardRef(function Alert(props, ref) {
+  return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+});
 const ProductDetails = () => {
   const { category, id } = useParams();
   const navigate = useNavigate();
   const [productDetails, setProductDetails] = useState(undefined);
   const [productImages, setProductImages] = useState([]);
   const [currentImage, setCurrentImage] = useState('');
+  const [state, setState] = React.useState({
+    open: false,
+    vertical: 'top',
+    horizontal: 'center',
+  });
+  const { vertical, horizontal, open } = state;
 
+  const handleClick = (newState) => {
+    setState({ ...newState, open: true });
+  };
+  const handleClose = () => {
+    setState({ ...state, open: false });
+  };
   useEffect(() => {
     categories.map((item) => {
       if (item.title === category) {
@@ -31,76 +49,93 @@ const ProductDetails = () => {
   const changeImage = (image) => {
     setCurrentImage(image);
   }
+
   return (
     productImages &&
     currentImage &&
     productDetails && (
       <Layout>
-        <div className="small-container single-product">
-          <div className="row">
-            <IconButton
-              sx={{ mb: 2, color: "#58869e" }}
-              onClick={() => navigate("/")}>
-              <ArrowBackIcon />
-            </IconButton>
-            <div className="col-2">
-              <img
-                src={require(`../assets/${currentImage}`)}
-                width="100%"
-              ></img>
-              <div className="small-img-row">
-                <div className="small-img-col">
+        <Store.Consumer>
+          {({ setCartItems }) => (
+            <div className="small-container single-product">
+              <div className="row">
+                <IconButton
+                  sx={{ mb: 2, color: "#58869e" }}
+                  onClick={() => navigate("/")}>
+                  <ArrowBackIcon />
+                </IconButton>
+                <div className="col-2">
                   <img
-                    src={require(`../assets/${productImages[0]}`)}
-                    onClick={() => changeImage(productImages[0])}
+                    src={require(`../assets/${currentImage}`)}
                     width="100%"
                   ></img>
-                  <img
-                    src={require(`../assets/${productImages[1]}`)}
-                    onClick={() => changeImage(productImages[1])}
-                    width="100%"
-                  ></img>
+                  <div className="small-img-row">
+                    <div className="small-img-col">
+                      <img
+                        src={require(`../assets/${productImages[0]}`)}
+                        onClick={() => changeImage(productImages[0])}
+                        width="100%"
+                      ></img>
+                      <img
+                        src={require(`../assets/${productImages[1]}`)}
+                        onClick={() => changeImage(productImages[1])}
+                        width="100%"
+                      ></img>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-2">
+                  <ul className="breadcrumbs">
+                    <li className="breadcrumbs-items">
+                      <a href="http://localhost:3000/" className="breadcrumbs-link">
+                        Home
+                      </a>
+                    </li>
+                    <li className="breadcrumbs-items">
+                      <a
+                        href="http://localhost:3000/Decor/products"
+                        className="breadcrumbs-link">
+                        Category
+                      </a>
+                    </li>
+                    <li className="breadcrumbs-items">
+                      <a href="" className="breadcrumbs-link breadcrumbs-link-active">
+                        {category && <p>{category}</p>}
+                      </a>
+                    </li>
+                  </ul>
+                  <h1 className="ProductTittle">{productDetails.tilte}</h1>
+                  <h4>${productDetails.price}</h4>
+                  <input type="number" min={1} max={5} defaultValue={1}></input>
+                  <div className="section-text">
+                    <a className="btn" onClick={() => {
+                      setCartItems(data => [...data, productDetails]);
+                      handleClick({ vertical: 'top', horizontal: 'right' });
+                    }}>
+                      Add To Cart
+                    </a>
+                    <Snackbar anchorOrigin={{ vertical, horizontal }}
+                      open={open}
+                      onClose={handleClose}
+                      message="I love snacks"
+                      key={vertical + horizontal}>
+                      <Alert onClose={handleClose} severity="success" sx={{ width: '100%' }}>
+                        Added to cart!
+                      </Alert>
+                    </Snackbar>
+                    <a className="btn-fav">
+                      &#10084;
+                    </a>
+                    <h3>Product Description</h3>
+                    <p className="ProductDescription">
+                      {productDetails.description}
+                    </p>
+                  </div>
                 </div>
               </div>
             </div>
-            <div className="col-2">
-              <ul className="breadcrumbs">
-                <li className="breadcrumbs-items">
-                  <a href="http://localhost:3000/" class="breadcrumbs-link">
-                    Home
-                  </a>
-                </li>
-                <li className="breadcrumbs-items">
-                  <a
-                    href="http://localhost:3000/Decor/products"
-                    class="breadcrumbs-link">
-                    Category
-                  </a>
-                </li>
-                <li className="breadcrumbs-items">
-                  <a href="" class="breadcrumbs-link breadcrumbs-link-active">
-                    {category && <p>{category}</p>}
-                  </a>
-                </li>
-              </ul>
-              <h1 className="ProductTittle">{productDetails.tilte}</h1>
-              <h4>${productDetails.price}</h4>
-              <input type="number" min={1} max={5} defaultValue={1}></input>
-              <div class="section-text">
-                <a href="" className="btn">
-                  Add To Cart
-                </a>
-                <a href="" className="btn-fav">
-                  &#10084;
-                </a>
-                <h3>Product Description</h3>
-                <p className="ProductDescription">
-                  {productDetails.description}
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
+          )}
+        </Store.Consumer>
       </Layout>
     )
   );
