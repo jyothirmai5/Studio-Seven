27dafb06a554111da5a067e3775daabdc713a972
[firstname lastname] <megantariaisyah@gmail.com>
2023-11-22 15:42:13 -0500

    added input field in footer for subscription

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -1,41 +1,76 @@
-import React from 'react';
-import './Footer.css'; 
+import React from "react";
+import "./Footer.css";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import { IconButton } from "@mui/material";
+import LanguageOutlinedIcon from "@mui/icons-material/LanguageOutlined";
 
 function Footer() {
-    return (<footer className="footer">
-        <div className="footer-content">
-            <div className="footer-section">
-                <h3>Help & Support</h3>
-                <ul>
-                    <li>FAQ</li>
-                    <li>Track Your Order</li>
-                    <li>Returns & Exchanges</li>
-                    <li>Customer Service</li>
-                </ul>
-            </div>
-            <div className="footer-section">
-                <h3>Company</h3>
-                <ul>
-                    <li>About Us</li>
-                    <li>Terms & Conditions</li>
-                    <li>Privacy Policy</li>
-                    <li>Careers</li>
-                </ul>
-            </div>
-            <div className="footer-section">
-                <h3>Connect With Us</h3>
-                <ul>
-                    <li>Facebook</li>
-                    <li>Twitter</li>
-                    <li>Instagram</li>
-                    <li>YouTube</li>
-                </ul>
-            </div>
+  return (
+    <footer className="footer">
+      <div className="footer-content">
+        <div className="footer-section">
+          <h3>Help & Support</h3>
+          <ul>
+            <li>FAQ</li>
+            <li>Track Your Order</li>
+            <li>Returns & Exchanges</li>
+            <li>Customer Service</li>
+          </ul>
         </div>
-        <div className="footer-bottom">
-            <p>&copy; 2023 Studio Seven</p>
+        <div className="footer-section">
+          <h3>Company</h3>
+          <ul>
+            <li>About Us</li>
+            <li>Terms & Conditions</li>
+            <li>Privacy Policy</li>
+            <li>Careers</li>
+          </ul>
         </div>
-    </footer>);
+        <div className="footer-section">
+          <h3>Connect With Us</h3>
+          <ul>
+            <li>Facebook</li>
+            <li>Twitter</li>
+            <li>Instagram</li>
+            <li>YouTube</li>
+          </ul>
+        </div>
+      </div>
+      <hr className="hr"></hr>
+      <div className="Subscription">
+        <form action="/action_page.php" id="form1">
+          <label className="Subscription-text" for="fname">
+            Subscribe to Our Shop
+          </label>
+          <input
+            className="Subscription-form"
+            type="text"
+            id="fname"
+            name="fname"
+          ></input>
+          <input
+            className="Subscription-btn"
+            type="submit"
+            value="Submit"
+          ></input>
+        </form>
+        <InstagramIcon
+          onClick={() => window.open("https://www.instagram.com", "_blank")}
+          fontSize="large"
+          className="icon"
+        ></InstagramIcon>
+        <LanguageOutlinedIcon
+          onClick={() => window.open("https://www.instagram.com", "_blank")}
+          fontSize="large"
+          className="icon"
+        ></LanguageOutlinedIcon>
+      </div>
+      <hr className="hr"></hr>
+      <div className="footer-bottom">
+        <p>&copy; 2023 Studio Seven</p>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
